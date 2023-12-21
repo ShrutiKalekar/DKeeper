7d8fe1e1dd08b8c10aad00e79150b1e15591fe3f
Shruti Kalekar <shrutikalekar090@gmail.com>
2023-12-21 20:24:17 +0530

    first commit

--- src/dkeeper/main.mo
+++ src/dkeeper/main.mo
@@ -1,5 +1,32 @@
-actor {
-  public func greet(name : Text) : async Text {
-    return "Hello, " # name # "!";
+import List "mo:base/List";
+import Debug "mo:base/Debug";
+
+actor Dkeeper {
+  public type Note = {
+    title: Text;
+    content: Text;
   };
-};
+
+  stable var notes: List.List<Note> = List.nil<Note>();
+
+  public func createNote(titleText: Text, contentText: Text) {
+    let newNote: Note = {
+      title = titleText;
+      content = contentText;
+    };
+
+    notes := List.push(newNote, notes);
+    Debug.print(debug_show(notes));
+  };
+
+  public query func readNotes(): async [Note]{
+    return List.toArray(notes);
+  };
+
+  public func deleteNote(id: Nat) {
+    var front = List.take(notes,id);
+    var back = List.drop(notes,id + 1);
+    notes := List.append(front,back);
+  };
+
+}
---
+++ src/dkeeper_assets/src/components/app.jsx
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from "react";
+import Header from "./header";
+import Footer from "./footer";
+import Note from "./note";
+import CreateArea from "./createArea";
+import { dkeeper } from "../../../declarations/dkeeper";
+
+function App() {
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote) {
+    setNotes(prevNotes => {
+      dkeeper.createNote(newNote.title, newNote.content)
+      return [newNote, ...prevNotes];
+    });
+  }
+
+  useEffect(() => {
+    fetchData();
+  },[]);
+
+  async function fetchData() {
+    const notesArray = await dkeeper.readNotes();
+    setNotes(notesArray);
+  }
+
+  function deleteNote(id) {
+    dkeeper.deleteNote(id);
+    setNotes(prevNotes => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
+
+  return (
+    <div>
+      <Header />
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
